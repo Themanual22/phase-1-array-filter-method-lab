f50dc2bac5841981a8d375a610911a0de993ed6d
Themanual22 <djamosry22@gmail.com>
2022-02-04 12:15:14 -0500

    commit tree 9f26b35be3b1c2f55717d4a42760823fd9b62741

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+// let name1 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+ console.log(findMatching)
+// function findMatching(){
+//  return findMatching.filter("name1");
+// }
+// const result = name1.filter(name1 => name1.length);
+// console.log(name1)
+function findMatching(source, sought) {
+  return source.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+  );
+}
+
+function fuzzyMatch(source, testString) {
+  return source.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(source, soughtName) {
+  return source.filter((record) => record.name === soughtName);
+}
